ef703256779c19a621ce483fe0b4aa091c831cc7
zhizhi1105 <91170856+zhizhi1105@users.noreply.github.com>
2021-09-29 12:54:14 +0800

    Add files via upload

---
+++ week03-5.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    printf("Hello%dWorld\n" ,300);
+    printf("%d %d    %d" , 300 ,200 ,100);
+}
+
+
---
+++ week03-6.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("");
+    scanf("%d", &a);
+
+    printf("%d\n" , a);
+}
+
