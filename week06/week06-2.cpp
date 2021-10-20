10ae60fcf204ef90608d4a3e7bcb0fc5ef47418a
zhizhi1105 <91170856+zhizhi1105@users.noreply.github.com>
2021-10-20 11:54:01 +0800

    Add files via upload

---
+++ week06/week06-2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+
+{
+    int a=999;
+    int b=100;
+    int temp;
+    printf("a:%d b:%d\n", a, b);
+
+    temp=a;
+    a=b;
+    b=a;
+    printf("a:%d b:%d\n", a,b);
+}
---
+++ week06/week06-4.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+ 	int a, b, c;
+ 	int temp;
+ 	scanf("%d %d %d",&a, &b ,&c);
+
+ 	if(a>c){
+ 		temp=a;
+ 		a=c;
+ 		c=temp;
+ 	}
+
+	if(b>c){
+		temp=b;
+		b=c;
+ 	    c=temp;
+	}
+	if(a+b<=c) printf("¿ù»~");
+	else if(a*a+b*b==c*c)printf("ª½¨¤");
+	else if(a*a+b*b<c*c)printf("¶w¨¤");
+	else if(a*a+b*b>c*c)printf("¾U¨¤");
+
+}
